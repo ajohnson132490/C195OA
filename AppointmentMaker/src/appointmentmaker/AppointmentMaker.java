0c20737c2af27d34bdcf50d9973489a08f56040f
LabUser <LabUser@DESKTOP-BT4131D>
2023-10-23 10:04:19 -0600

    Property files work, whole project compiles

--- AppointmentMaker/src/appointmentmaker/AppointmentMaker.java
+++ AppointmentMaker/src/appointmentmaker/AppointmentMaker.java
@@ -4,10 +4,7 @@
  */
 package appointmentmaker;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,6 +13,10 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import static java.lang.System.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import static javafx.application.Application.launch;
 
 /**
  *
@@ -25,26 +26,45 @@ public class AppointmentMaker extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        Locale locale = new Locale("fr", "FR");
+        ResourceBundle lang = ResourceBundle.getBundle("appointmentmaker.lang", locale);
         String url = "jdbc:mysql://localhost:3306/client_schedule";
         String username = "sqlUser";
         String pass = "Passw0rd!";
         try {
         Connection conn = DriverManager.getConnection(url, username, pass);
+        Statement stmt = conn.createStatement();       // Create Statement
+        String query = "SELECT * FROM customers";
+        ResultSet rs = stmt.executeQuery(query);       // Execute Query
+        while (rs.next()) {                            // Process Results
+            out.print(rs.getInt("Customer_ID") + "  ");   // Print Columns
+            out.print(rs.getString("Customer_Name") + "  ");
+            out.print(rs.getString("Address") + "  ");
+            out.print(rs.getString("Postal_Code") + "  ");
+            out.println(rs.getString("Create_Date"));
+        }
         } catch (Exception e) {
             System.out.println(e);
         }
+        
+        StackPane root = new StackPane();
+
+        
+        try {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println(lang.getString("greeting"));
             }
         });
-        
-        StackPane root = new StackPane();
         root.getChildren().add(btn);
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+        }
+        
         
         Scene scene = new Scene(root, 300, 250);
         
