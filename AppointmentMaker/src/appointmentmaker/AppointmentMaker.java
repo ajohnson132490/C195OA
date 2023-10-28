a557fa540dcd35de03639df8e9fbe0bf17f7eb7a
LabUser <LabUser@DESKTOP-BT4131D>
2023-10-28 13:04:57 -0600

    Login form front end

--- AppointmentMaker/src/appointmentmaker/AppointmentMaker.java
+++ AppointmentMaker/src/appointmentmaker/AppointmentMaker.java
@@ -10,13 +10,18 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import static java.lang.System.*;
 import java.lang.reflect.InvocationTargetException;
+import java.time.ZoneId;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import static javafx.application.Application.launch;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 /**
  *
@@ -26,49 +31,91 @@ public class AppointmentMaker extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        //Set Language
         Locale locale = new Locale("fr", "FR");
         ResourceBundle lang = ResourceBundle.getBundle("appointmentmaker.lang", locale);
+        
+        //Connecting to the database
         String url = "jdbc:mysql://localhost:3306/client_schedule";
         String username = "sqlUser";
         String pass = "Passw0rd!";
         try {
         Connection conn = DriverManager.getConnection(url, username, pass);
-        Statement stmt = conn.createStatement();       // Create Statement
-        String query = "SELECT * FROM customers";
-        ResultSet rs = stmt.executeQuery(query);       // Execute Query
-        while (rs.next()) {                            // Process Results
-            out.print(rs.getInt("Customer_ID") + "  ");   // Print Columns
-            out.print(rs.getString("Customer_Name") + "  ");
-            out.print(rs.getString("Address") + "  ");
-            out.print(rs.getString("Postal_Code") + "  ");
-            out.println(rs.getString("Create_Date"));
+        //Get some sample data
+        Statement stmt = conn.createStatement();
+        String query = "SELECT * FROM users";
+        ResultSet rs = stmt.executeQuery(query);
+        //Print all results
+        while (rs.next()) {
+            out.print(rs.getInt("User_ID") + "  ");
+            out.print(rs.getString("User_Name") + "  ");
+            out.print(rs.getString("Password") + "  \n");
         }
         } catch (Exception e) {
             System.out.println(e);
         }
         
-        StackPane root = new StackPane();
-
+        //Create the main VBox
+        VBox mainVBox = new VBox();
         
-        try {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        
+        //Title
+        Label title = new Label("Appointment Maker");
+        title.setStyle("-fx-font: 24 ariel;");
+        mainVBox.getChildren().add(title);
+        
+        //Username and Password fields
+        VBox form = new VBox();
+        form.getStyleClass().add("loginForm");
+        
+        Label uName = new Label("Username");
+        TextField uField = new TextField();
+        
+        Label pWord = new Label("Password");
+        TextField pField = new TextField();
+        
+        //Login Button
+        HBox loginBtnPadding = new HBox();
+        loginBtnPadding.setPadding(new Insets(10, 0, 0, 150));
+        Button loginBtn = new Button("Login");
+        loginBtn.setPrefWidth(50);
+        loginBtnPadding.getChildren().add(loginBtn);
+        loginBtn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
                 System.out.println(lang.getString("greeting"));
             }
         });
-        root.getChildren().add(btn);
-        } catch (Exception e) {
-            System.out.println(e.getCause());
-        }
+        
+        //Add Form parts to form VBox
+        form.getChildren().add(uName);
+        form.getChildren().add(uField);
+        form.getChildren().add(pWord);
+        form.getChildren().add(pField);
+        form.getChildren().add(loginBtnPadding);
+        mainVBox.getChildren().add(form);
+        
+        //User Location
+        HBox lower = new HBox();
+        lower.setPadding(new Insets(70, 0, 0, 150));
+        ZoneId zoneID = ZoneId.systemDefault();
+        Label location = new Label("Location: " + zoneID.toString());
+        location.setPrefWidth(150);        
+        lower.getChildren().add(location);
+        mainVBox.getChildren().add(lower);
         
         
-        Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        //Add all items to the root
+        Pane root = new Pane();
+        root.getChildren().add(mainVBox);
+        mainVBox.getStyleClass().add("loginRoot");
+        
+        //Create Scene
+        Scene scene = new Scene(root, 350, 400);
+        scene.getStylesheets().add(getClass().getResource("resources/stylesheet.css").toExternalForm());
+        primaryStage.setTitle("Login");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
