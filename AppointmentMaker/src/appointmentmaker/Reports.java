7979157931126c61890e557483cb2efc3dd395f2
ajohnson132490 <ajohnson132490@gmail.com>
2024-08-19 21:34:07 -0400

    added column setting function for viewReports page

--- AppointmentMaker/src/appointmentmaker/Reports.java
+++ AppointmentMaker/src/appointmentmaker/Reports.java
@@ -1,7 +1,12 @@
 package appointmentmaker;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -9,12 +14,15 @@ import java.sql.ResultSet;
 public class Reports {
     private Connection conn;
 
+    public void setConnection(Connection conn) {
+        this.conn = conn;
+    }
+
     public ObservableList<ObservableList> appointmentsReport() {
         ObservableList<ObservableList> appointmentData = FXCollections.observableArrayList();
         try {
             //Get all the types and months
-            String query = "SELECT DISTINCT type, Month(start), count(*) AS QTY FROM appointments\n" +
-                    "GROUP BY type;";
+            String query = "SELECT DISTINCT type, Month(start), count(*) AS QTY FROM appointments GROUP BY type";
             ResultSet rs = conn.createStatement().executeQuery(query);
 
             while(rs.next()) {
@@ -69,9 +77,9 @@ public class Reports {
                         //Add the data to the row
                         row.add(rs.getString(i));
                     }
-                    //Add the full row to the observableList
-                    appointmentData.add(row);
                 }
+                //Add the full row to the observableList
+                appointmentData.add(row);
             }
         } catch (Exception e) {
             System.out.println("appointmentsReport: " + e);
@@ -84,8 +92,8 @@ public class Reports {
         ObservableList<ObservableList> appointmentData = FXCollections.observableArrayList();
         try {
             //Get all the types and months
-            String query = "SELECT Contact_ID, Appointment_ID, Title, Type, Description, Start, End, Customer_ID FROM appointments \n" +
-                    "ORDER BY Contact_ID, Start;";
+            String query = "SELECT Contact_ID, Appointment_ID, Title, Type, Description, Start, End, Customer_ID FROM appointments " +
+                    "ORDER BY Contact_ID, Start";
             ResultSet rs = conn.createStatement().executeQuery(query);
 
             while(rs.next()) {
@@ -95,15 +103,16 @@ public class Reports {
                     //Add the data to a row
                     if (rs.getMetaData().getColumnName(i).equals("Contact_ID")) {
                         AppointmentHelpers h = new AppointmentHelpers();
+                        h.setConnection(conn);
                         //Get the contacts name and add it to the appointment
                         row.add(h.getContact(rs.getInt("Contact_ID")));
                     } else {
                         //Add the data to the row
                         row.add(rs.getString(i));
                     }
-                    //Add the full row to the observableList
-                    appointmentData.add(row);
                 }
+                //Add the full row to the observableList
+                appointmentData.add(row);
             }
         } catch (Exception e) {
             System.out.println("appointmentsReport: " + e);
@@ -116,10 +125,10 @@ public class Reports {
         ObservableList<ObservableList> appointmentData = FXCollections.observableArrayList();
         try {
             //Get all the types and months
-            String query = "SELECT customers.Customer_ID, customers.Customer_Name, countries.Country FROM customers\n" +
-                    "LEFT JOIN first_level_divisions ON customers.Division_ID = first_level_divisions.Division_ID\n" +
-                    "LEFT JOIN countries on first_level_divisions.Country_ID = countries.Country_ID\n" +
-                    "ORDER BY countries.Country;";
+            String query = "SELECT customers.Customer_ID, customers.Customer_Name, countries.Country FROM customers " +
+                    "LEFT JOIN first_level_divisions ON customers.Division_ID = first_level_divisions.Division_ID " +
+                    "LEFT JOIN countries on first_level_divisions.Country_ID = countries.Country_ID " +
+                    "ORDER BY countries.Country";
             ResultSet rs = conn.createStatement().executeQuery(query);
 
             while(rs.next()) {
@@ -127,15 +136,88 @@ public class Reports {
                 for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                     //Add the data to the row
                     row.add(rs.getString(i));
-
-                    //Add the full row to the observableList
-                    appointmentData.add(row);
                 }
+                //Add the full row to the observableList
+                appointmentData.add(row);
             }
         } catch (Exception e) {
             System.out.println("appointmentsReport: " + e);
         }
 
         return appointmentData;
+    }
+
+    public void setColumns(TableView<ObservableList> table, int type) {
+        try {
+            table.getColumns().clear();
+            String query = "";
+            ResultSet rs;
+            switch (type) {
+                case 1:
+                    //Query the database
+                    query = "SELECT DISTINCT type, Month(start), count(*) AS QTY FROM appointments " +
+                            "GROUP BY type";
+                    rs = conn.createStatement().executeQuery(query);
+                    //Populate the table with columns
+                    for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                        final int finI = i;
+                        //Create a new column
+                        TableColumn col = new TableColumn<>();
+                        col.setText(rs.getMetaData().getColumnName(i + 1));
+                        col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                            @Override
+                            public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                                return new SimpleStringProperty(param.getValue().get(finI).toString());
+                            }
+                        });
+                        table.getColumns().add(col);
+                    }
+                    break;
+                case 2:
+                    //Query the database
+                    query = "SELECT Contact_ID, Appointment_ID, Title, Type, Description, Start, End, Customer_ID FROM appointments " +
+                            "ORDER BY Contact_ID, Start";
+                    rs = conn.createStatement().executeQuery(query);
+                    //Populate the table with columns
+                    for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                        final int finI = i;
+                        //Create a new column
+                        TableColumn col = new TableColumn<>();
+                        col.setText(rs.getMetaData().getColumnName(i + 1));
+                        col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                            @Override
+                            public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                                return new SimpleStringProperty(param.getValue().get(finI).toString());
+                            }
+                        });
+                        table.getColumns().add(col);
+                    }
+                    break;
+                case 3:
+                    //Query the database
+                    query = "SELECT customers.Customer_ID, customers.Customer_Name, countries.Country FROM customers " +
+                            "LEFT JOIN first_level_divisions ON customers.Division_ID = first_level_divisions.Division_ID " +
+                            "LEFT JOIN countries on first_level_divisions.Country_ID = countries.Country_ID " +
+                            "ORDER BY countries.Country";
+                    rs = conn.createStatement().executeQuery(query);
+                    //Populate the table with columns
+                    for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                        final int finI = i;
+                        //Create a new column
+                        TableColumn col = new TableColumn<>();
+                        col.setText(rs.getMetaData().getColumnName(i + 1));
+                        col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                            @Override
+                            public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                                return new SimpleStringProperty(param.getValue().get(finI).toString());
+                            }
+                        });
+                        table.getColumns().add(col);
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+
+        }
     }
 }
